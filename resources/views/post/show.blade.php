1f6f162249172158e3a8fdc1451e5d99c0b20187
BuildTools <unconfigured@null.spigotmc.org>
2023-11-24 19:07:32 +0100

    Eye

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,7 +3,7 @@
         {{ $post->user->username }}
     </div>
     <div class="mb-4 text-xs text-gray-500">
-        <img src="{{ asset($post->img_path) }}" alt="Post Image" class="mb-4 rounded-md">
+        <img src="{{ asset('storage/' . $post->img_path) }}" alt="Post Image" class="mb-4 rounded-md">
         {{ $post->published_at }}
     </div>
     <div>
@@ -16,14 +16,5 @@
           <div class="text-gray-700">{{ $post->user->name }}</div>
           <div class="text-gray-500">{{ $post->user->email }}</div>
         </div>
-      </div>
-
-      <div class="mt-8 flex items-center justify-center">
-        <a
-          href="{{ route('front.post.index') }}"
-          class="font-bold bg-white text-gray-700 px-4 py-2 rounded shadow"
-        >
-          Retour à la liste des post
-        </a>
       </div>
 </x-guest-layout>
